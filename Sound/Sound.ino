3b7d31bf7a1a586bdf06a144c5d1834fa9949b4d
Batophobia <godrox3@yahoo.com>
2022-01-23 21:04:14 -0600

    this was a triumph

---
+++ Sound/Sound.ino
@@ -0,0 +1,44 @@
+int speakerPin = 9;
+
+#define NUM_NOTES 29
+String notes = "gfeef            agfeef  d ea";// space represents a rest
+int tempo = 150;
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
+
+void setup() {
+  pinMode(speakerPin, OUTPUT);
+}
+
+void loop() {
+  for (int i = 0; i < NUM_NOTES; i++) {
+    if (notes[i] == ' ') {
+      delay(tempo); // rest
+    } else {
+      playNote(notes[i], tempo);
+    }
+    
+    delay(tempo / 2); 
+  }
+  
+    delay(5000);
+}
