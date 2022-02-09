c0a775205840971d3d0d10a09692c402de5ab44d
Batophobia <godrox3@yahoo.com>
2022-02-08 23:11:55 -0600

    Color matching game

--- Esplora/Esplora.ino
+++ Esplora/Esplora.ino
@@ -2,6 +2,9 @@
 #include <Esplora.h>
 
 #define DELAY 16 // approx. 60 fps
+#define CAPTURE_NOTE 550
+#define LOSE_NOTE 100
+#define BUFFER 10
 
 #define DATA_PIN   11
 #define NUM_LEDS    40
@@ -25,9 +28,17 @@ int plrPos = 0;
 int plrClr = 0;
 int plrBright = 0;
 int plrSat = 255;
-int deltSat = -1;
 
-int enemyClr = 128;
+#define MAX_ENEMY 3
+#define SPAWN_RATE 120
+
+int enemyPos[] = { -1, -1, -1 };
+int enemyClr[] = { -1, -1, -1 };
+int nextSpawn = SPAWN_RATE;
+
+bool isPlaying = false;
+int idleIdx = 0;
+int idleDir = 1;
 
 void resetLEDS() {
   for (int i = 0; i < NUM_LEDS; i++) {
@@ -42,36 +53,105 @@ void setupLEDs() {
   FastLED.show();
 }
 
+void setupGame() {
+  isPlaying = false;
+  setupEnemies();
+  setupPlayer();
+  idleIdx = 0;
+  idleDir = 1;
+}
+
+void setupEnemies() {
+  for(int i = MAX_ENEMY; --i >= 0;) {
+    enemyPos[i] = -1;
+    enemyClr[i] = -1;
+  }
+  nextSpawn = SPAWN_RATE;
+}
+
+void setupPlayer() {
+  plrPos = round(NUM_LEDS/2);
+}
+
 void setup() {
+  //Serial.begin(19200);
   setupLEDs();
+  setupGame();
 }
 
 void loop() {
+  Esplora.noTone();
   handleButtons();
   handleSlider();
   handleLightSensor();
-  test();
+  handleTemp();
+  
+  if(isPlaying) {
+    enemySpawner();
+    resetLEDS();
+    drawPlayer();
+    drawEnemies();
+  } else {
+    idle();
+  }
+  FastLED.show();
+  
+  //test();
   delay(DELAY);
 }
 
-void test() {
-  resetLEDS();
-  // Player test
+void idle(){
+  leds[idleIdx] = CHSV(0, 0, 0);
+  idleIdx += idleDir;
+  if(idleIdx <= 0 || idleIdx >= NUM_LEDS - 1) idleDir *= -1;
+  leds[idleIdx] = CHSV(plrClr, plrSat, plrBright);
+}
+
+void drawPlayer() {
   leds[plrPos] = CHSV(plrClr, plrSat, plrBright);
-  FastLED.show();
+}
 
-  // Enemy test
-  leds[2] = CHSV(enemyClr, plrSat, plrBright);
-  FastLED.show();
+void drawEnemies() {
+  for(int i = MAX_ENEMY; --i >= 0;) {
+    if(enemyPos[i] > -1)
+      leds[enemyPos[i]] = CHSV(enemyClr[i], plrSat, plrBright);
+  }
+}
 
-  plrSat += deltSat;
-  if(plrSat <= 0 || plrSat >= 255) deltSat *= -1;
+void enemySpawner() {
+  //Serial.print("Next spawn: "); Serial.println(nextSpawn);
+  if(nextSpawn-- > 0) return;
+
+  //Serial.println("SPAWN ENEMY");
+  nextSpawn = SPAWN_RATE;
+  for(int i = MAX_ENEMY; --i >= 0;) {
+    if(enemyPos[i] > -1) continue;
+    
+    int t = random(0,2);
+    if(plrPos < 4) t = 1;
+    else if(plrPos > NUM_LEDS-4) t = -1;
+    else t = (t * 2) - 1; // Get 1 or -1
+    
+    t = t * random(2,4);
+
+    bool badLoc = false;
+    for(int j = 0; j < MAX_ENEMY; j++)
+      if(enemyPos[j] == plrPos + t) badLoc = true;
+    if(badLoc) continue;
+    
+    enemyPos[i] = plrPos + t;
+
+    // Didn't want to deal with wrap-around maths
+    t = random(BUFFER, 255 - BUFFER);
+    enemyClr[i] = t;
+    return;
+  }
 }
 
 void handleSlider() {
   int slider = Esplora.readSlider();
   plrClr = slider / 4;
-  enemyClr = (plrClr + 128) % 256;
+  //enemyClr = (plrClr + 128) % 256;
 }
 
 void handleLightSensor() {
@@ -79,6 +159,14 @@ void handleLightSensor() {
   int brightness = map(light, 0, 1024, 0, 128);
   brightness = constrain(brightness, 0, 128);
   plrBright = 255 - brightness;
+}
+
+void handleTemp() {
+  int temp = Esplora.readTemperature(DEGREES_F);
+  //Serial.print("Temp: "); Serial.println(temp);
+  int saturation = map(temp, 65, 85, 125, 255);
+  saturation = constrain(saturation, 125, 255);
+  plrSat = saturation;
 }
 
 void handleButtons() {
@@ -96,6 +184,11 @@ void handleButtons() {
 }
 
 void buttonPress(byte button, bool pressed) {
+  if(!isPlaying) {
+    if(pressed && ( buttons[button] == SWITCH_UP || buttons[button] == SWITCH_DOWN ))
+      isPlaying = true;
+    return;
+  }
   switch(buttons[button]) {
     case JOYSTICK_DOWN:
     case JOYSTICK_RIGHT:
@@ -112,4 +205,36 @@ void buttonPress(byte button, bool pressed) {
     case SWITCH_UP:
       break;
   }
+  checkHit();
+}
+
+void checkHit() {
+  for(int i = MAX_ENEMY; --i >= 0;) {
+    if(plrPos != enemyPos[i]) continue;
+
+    //Serial.print("Player: ");Serial.print(plrClr);Serial.print(" - Enemy: ");Serial.println(enemyClr[i]);
+
+    if(enemyClr[i] - BUFFER <= plrClr && plrClr <= enemyClr[i] + BUFFER) return destroy(i);
+    else return lose();
+  }
+}
+
+void destroy(int idx) {
+  Esplora.tone(CAPTURE_NOTE);
+  enemyPos[idx] = -1;
+  enemyClr[idx] = -1;
+}
+
+void lose() {
+  for(int i = NUM_LEDS; --i >= 0;) {
+    Esplora.tone(LOSE_NOTE + i);
+    leds[i] = CHSV(0, plrSat, plrBright);
+    FastLED.show();
+    delay(30);
+  }  
+  Esplora.noTone();
+
+  
+  setupGame();
+  //Serial.println("Game Over");
 }
